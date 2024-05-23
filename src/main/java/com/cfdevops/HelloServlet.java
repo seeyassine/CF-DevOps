d02a993ba5e14a2ff99e5af102d9b86f09309c61
See Yassine <seeyassine@gmail.com>
2024-05-23 14:53:48 +0100

    add message

--- src/main/java/com/cfdevops/HelloServlet.java
+++ src/main/java/com/cfdevops/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Bonjour et bon courage dans votre examen en DevOps";
+        message = "hello world";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -19,6 +19,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("message 2");
         out.println("</body></html>");
     }
 
